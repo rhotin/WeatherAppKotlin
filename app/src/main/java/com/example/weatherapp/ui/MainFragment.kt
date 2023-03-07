69030bcd85c0f4d52d8a12401239c692c1a5ab45
r47 <roman.hotin.rh@gmail.com>
2023-03-07 11:36:30 -0500

    optimize imports

--- app/src/main/java/com/example/weatherapp/ui/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainFragment.kt
@@ -1,12 +1,9 @@
 package com.example.weatherapp.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.example.weatherapp.R
